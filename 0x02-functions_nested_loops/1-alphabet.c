db24cd0fd67430cb587c1222b16471a15844279b
Rexpass <robeleffa123@gmail.com>
2022-12-13 06:57:02 -0800

    Main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all lowercase letters
+ *
+ * Return: null
+ */
+
+void print_alphabet(void)
+{
+	int stop = 122, count = 97;
+	while (count <= stop)
+	{
+		_putchar(count):
+		count++;
+	}
+	_putchar('\n');
+}
