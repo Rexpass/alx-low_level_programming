f33ad1f0fd4f7d486bf0a60b044ab52b089cb99a
Rexpass <robeleffa123@gmail.com>
2022-12-14 07:18:12 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the number of the sign to be printed
- * Return: 1 if number is greater than zero
- * 0 of number is zero
- * -1 if number is less than zero
+ * print_sign - checks the sign of n and print it
+ *
+ * @n: any number as para
+ * Return: 1 if n is +ve, 0 if n is 0, -1 if n is -ve.
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
 	}
 	else
 	{
