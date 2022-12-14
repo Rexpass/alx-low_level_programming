0450d392d38858ec7d64f1900a46a40b3372fc5e
Rexpass <robeleffa123@gmail.com>
2022-12-14 08:39:54 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,13 @@
 #include "main.h"
+/**
+ *
+ *  * times_table - function that prints the 9 times table, starting with 0
+ * @void: no argument
+ */
+void times_table(void)
+{
+	int i,j,k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j<
