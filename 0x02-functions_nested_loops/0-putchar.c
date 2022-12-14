2198f4323177069caf641dcca0c6acb5b7c0a3cb
Rexpass <robeleffa123@gmail.com>
2022-12-14 06:30:40 -0800

    Main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,14 @@
 
 int main(void)
 {
-	char *word = "_putchar";
-	while (*word != '\0')
-	{
-		_putchar(*word);
-		word++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
