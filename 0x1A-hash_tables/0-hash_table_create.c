be3b8c42e0b9acfe468214a2dc54c404c0157b9c
Robel Effa <96461347+Rexpass@users.noreply.github.com>
2023-04-08 15:52:59 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,32 @@
 #include "hash_tables.h"
 
-/**
- * hash_table_create - creates a hash table
- * @size: size of the array of the hash table
- *
- * Return: Pointer to the newly created hash table
- */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *ht;
+typedef struct {
+    /* your data structure to store key-value pairs */
+} hash_table_t;
 
-	if (size == 0)
-		return (NULL);
-
-	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
-		return (NULL);
-
-	ht->size = size;
-	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-	if (ht->array == NULL)
-		return (NULL);
-
-	return (ht);
+hash_table_t *hash_table_create(unsigned long int size) {
+    /* allocate memory for hash table */
+    hash_table_t *ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL) {
+        return NULL;
+    }
+    
+    /* initialize the hash table */
+    /* allocate memory for the array */
+    ht->array = malloc(size * sizeof(/* type of array element */));
+    if (ht->array == NULL) {
+        free(ht);
+        return NULL;
+    }
+    /* initialize array elements */
+    for (unsigned long int i = 0; i < size; i++) {
+        /* initialize each element of the array */
+    }
+    /* set other fields of the hash table */
+    ht->size = size;
+    /* ... */
+    
+    return ht;
 }
+
+
